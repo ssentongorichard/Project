699378b6e898a1a358ef1010b2ec0ff4c6aee538
Ssentongo Richard <richardssentongo111@gmail.com>
2022-10-28 16:08:30 +0300

    my project

---
+++ models/Urbanfarmerupload.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+productcode: {
+  type: String
+},
+productcategory: {
+  type: String
+  
+},
+productname: {
+  type: String
+},
+producttype: {
+  type: String
+},
+productimage: {
+  type: String
+},
+stockcount: {
+  type: Number
+},
+stockmeasure: {
+  type: String
+},
+quantity: {
+  type: String
+},
+unitprice: {
+  type: Number
+},
+email: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'Registration'
+  
+},
+uniquenum: {
+  type: String
+},
+datetoday: {
+  type: Date
+},
+ward: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'Registration'
+  
+},
+pytmode: {
+  type: String
+},
+deliverymode: {
+  type: String
+},
+status: {
+  type: String,
+  default: "Pending",
+  enum: ['Pending', 'Approved']
+},
+availability: {
+  type: String, 
+  default: "available",
+  enum: ["available", "booked", "N/A"] 
+  }
+})
+
+module.exports = mongoose.model('UrbanFarmerUpload', userSchema);
---
+++ routes/dashboardRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+// const multer = require('multer');
+const connectEnsureLogin = require('connect-ensure-login');
+
+//Importing model
+// const Registration = require('../models/User');
+// const UrbanFarmerUpload = require('../models/Urbanfarmerupload');
+
+//structure of a route
+router.get('/dashboard',connectEnsureLogin.ensureLoggedIn(), (req,res) =>{
+res.render('dashboardAO');
+})
+router.post('/dashboard',async(req,res) =>{
+    console.log(req.body);
+   
+});
+router.get('/dashboardfo',connectEnsureLogin.ensureLoggedIn(), (req,res) =>{
+    res.render('dashboardFO');
+    })
+    router.post('/dashboardfo',async(req,res) =>{
+        console.log(req.body);
+       
+    });
+    router.get('/dashboarduf',connectEnsureLogin.ensureLoggedIn(), (req,res) =>{
+        res.render('dashboardUF');
+        })
+        router.post('/dashboarduf',async(req,res) =>{
+            console.log(req.body);
+           
+        });
+
+
+//This is the last line always
+module.exports = router;
